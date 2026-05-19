a471b267c1d10a393dd205e2af3c2cce18a10c66
Ishaq Ahmed <ishaqakund01@gmail.com>
2026-05-19 22:03:09 +0300

    admin user management (src/admin/) index.php

--- src/admin/api/index.php
+++ src/admin/api/index.php
@@ -100,14 +100,15 @@ function getUsers($db) {
     //       Do NOT select the password column.
     $sql = "SELECT id, name, email, is_admin, created_at FROM users";
     $params = [];
-    $search = $_GET['search'] ?? '';
+
+    $searchTerm = isset($_GET['search']) ? trim($_GET['search']) : '';
 
     // TODO: If the 'search' query parameter is present, append a WHERE clause:
     //       WHERE name LIKE :search OR email LIKE :search
     //       Wrap the search term with '%' wildcards when binding.
-    if ($search) {
+    if ($searchTerm !== '') {
         $sql .= " WHERE name LIKE :search OR email LIKE :search";
-        $params['search'] = "%$search%";
+        $params['search'] = "%" . $searchTerm . "%";
     }
     // TODO: If the 'sort' query parameter is present and is one of the allowed
     //       fields (name, email, is_admin), append an ORDER BY clause.
